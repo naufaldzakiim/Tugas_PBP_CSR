0923eb64403463854e0db8126daa25ccb8392ca5
Faizal_Husain_Adiasha <adihusain0@gmail.com>
2023-09-03 21:11:43 +0700

    restruct from then to async await function

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,18 @@
-const searchBtn = document.getElementById('search-btn');
-const foodList = document.getElementById('food');
-const foodDetailsContent = document.querySelector('.food-details-content');
-const recipeCloseBtn = document.getElementById('recipe-close-btn');
+const searchBtn = document.getElementById("search-btn");
+const foodList = document.getElementById("food");
+const foodDetailsContent = document.querySelector(".food-details-content");
+const recipeCloseBtn = document.getElementById("recipe-close-btn");
 
-function getFoodList() {
-    let searchInput = document.getElementById('search-input').value.trim();
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`)
-    .then(response => response.json())
-    .then(data => {
-        let html = "";
-        if (data.meals) {
-            data.meals.forEach(meal => {
-                html += `
+async function getFoodList() {
+  let searchInput = document.getElementById("search-input").value.trim();
+  const response = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`
+  );
+  const data = await response.json();
+  let html = "";
+  if (data.meals) {
+    data.meals.forEach((meal) => {
+      html += `
                     <div class="food-item neumorp-card" data-id="${meal.idMeal}">
                         <div class="food-image">
                             <img src="${meal.strMealThumb}" alt="food">
@@ -22,27 +23,26 @@ function getFoodList() {
                         </div>
                     </div>
                 `;
-            });
-            foodList.classList.remove('not-found');
-        }
-        
-        else {
-            html = "Makanan tidak ditemukan";
-            foodList.classList.add('not-found');
-        }
-
-        foodList.innerHTML = html;
     });
+    foodList.classList.remove("not-found");
+  } else {
+    html = "Makanan tidak ditemukan";
+    foodList.classList.add("not-found");
+  }
+
+  foodList.innerHTML = html;
 }
 
-const foodListInit = () => {
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=`)
-    .then(response => response.json())
-    .then(data => {
-        let html = "";
-        if (data.meals) {
-            data.meals.forEach(meal => {
-                html += `
+async function foodListInit() {
+  const response = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/search.php?s=`
+  );
+  const data = await response.json();
+  if (data.meals) {
+    let html = "";
+    if (data.meals) {
+      data.meals.forEach((meal) => {
+        html += `
                     <div class="food-item neumorp-card" data-id="${meal.idMeal}">
                         <div class="food-image">
                             <img src="${meal.strMealThumb}" alt="food">
@@ -53,17 +53,16 @@ const foodListInit = () => {
                         </div>
                     </div>
                 `;
-            });
-        }
-
-        foodList.innerHTML = html;
-    });
-};
+      });
+    }
+    foodList.innerHTML = html;
+  }
+}
 
 function foodRecipeModal(food) {
-    console.log(food);
-    food = food[0];
-    let html = `
+  console.log(food);
+  food = food[0];
+  let html = `
         <h2 class="recipe-title">${food.strMeal}</h2>
         <div class="recipe-food-img">
             <img src="${food.strMealThumb}" alt="food">
@@ -77,25 +76,27 @@ function foodRecipeModal(food) {
             <a href="${food.strYoutube}" target="_blank">Watch Video</a>
         </div>
     `;
-    
-    foodDetailsContent.innerHTML = html;
-    foodDetailsContent.parentElement.classList.add('show-recipe');
+
+  foodDetailsContent.innerHTML = html;
+  foodDetailsContent.parentElement.classList.add("show-recipe");
 }
 
-function getFoodRecipe(e) {
-    e.preventDefault();
-    if (e.target.classList.contains('recipe-btn')) {
-        let foodItem = e.target.parentElement.parentElement;
-        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${foodItem.dataset.id}`)
-        .then(response => response.json())
-        .then(data => foodRecipeModal(data.meals));
-    }
+async function getFoodRecipe(e) {
+  e.preventDefault();
+  if (e.target.classList.contains("recipe-btn")) {
+    let foodItem = e.target.parentElement.parentElement;
+    const response = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${foodItem.dataset.id}`
+    );
+    const data = await response.json();
+    foodRecipeModal(data.meals);
+  }
 }
 
-searchBtn.addEventListener('click', getFoodList);
-foodList.addEventListener('click', getFoodRecipe);
-recipeCloseBtn.addEventListener('click', () => {
-    foodDetailsContent.parentElement.classList.remove('show-recipe');
+searchBtn.addEventListener("click", getFoodList);
+foodList.addEventListener("click", getFoodRecipe);
+recipeCloseBtn.addEventListener("click", () => {
+  foodDetailsContent.parentElement.classList.remove("show-recipe");
 });
 
 foodListInit();
